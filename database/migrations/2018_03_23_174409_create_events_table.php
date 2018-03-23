7dd7dd083cda8ed2fff3820245670a4a9986cd20
Kirimson <kierangates@outlook.com>
2018-03-23 18:53:07 +0000

    Initial Project

---
+++ database/migrations/2018_03_23_174409_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name', 255);
+            $table->string('description', 255);
+            $table->timestamp('time')->nullable();
+            $table->binary('picture');
+            $table->integer('organiser_id');
+            $table->string('contact', 255);
+            $table->string('venue', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
