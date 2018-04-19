6f023f40cac89eacb9278ebc34baf2e59d53ada2
Kirimson <kierangates@outlook.com>
2018-04-19 22:26:08 +0100

    move root AGAIN

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -62,8 +62,8 @@
     </div>
 
     {{-- Start description/details section --}}
-    <div class="row centered-items">
-        <div class="col-lg-6">
+    <div class="row">
+        <div class="col-lg-12">
             <h1>Description</h1>
 
             {{-- If creating, display textarea for description, else display description --}}
