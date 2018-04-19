3a65655e67461c0d9b2be352be1bafd612641cef
Kirimson <kierangates@outlook.com>
2018-04-19 19:16:14 +0100

    restore old root
    dont really want access to .env

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,12 +35,7 @@ require __DIR__.'/vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
-
-// Add After $app = require_once __DIR__.'/../bootstrap/app.php';
-$app->bind('path.public', function() {
-	return __DIR__;
-});
+$app = require_once __DIR__.'/../bootstrap/app.php';
 /*
 |--------------------------------------------------------------------------
 | Run The Application
